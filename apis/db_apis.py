9616317b569c3d4a403046d67569f3da7f88c041
Srinivas Chavan <srinivas@platform9.com>
2022-08-23 08:21:22 +0000

    aaa

--- apis/db_apis.py
+++ apis/db_apis.py
@@ -3,7 +3,7 @@ Flask App to provide API wrappers over the Failure DB backend
 """
 import os
 import json
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, render_template
 from flask_mysqldb import MySQL
 
 app = Flask(__name__)
@@ -15,7 +15,7 @@ app.config['MYSQL_DB'] = 'failure_db'
 
 mysql = MySQL(app)
 
-def _list_items(table):
+def _list_items(table, return_json=False):
     """
     Generic method for all GET API calls. User can specify query params to the get call.
     The keys used for query are same as those documented in corresponding POST calls
@@ -36,6 +36,31 @@ def _list_items(table):
         json_data=[]
         for result in return_values:
             json_data.append(dict(zip(row_headers,result)))
+        if return_json:
+            return json_data
+        return jsonify(json_data)
+    except Exception as err:
+        return jsonify({"status": "failed", "details": str(err)})
+
+def _get_item_by_id(table, id, return_json=False):
+    """
+    Generic method for all GET API calls. User can specify query params to the get call.
+    The keys used for query are same as those documented in corresponding POST calls
+    """
+    try:
+        print(request.args)
+        query = f"SELECT * from {table} where id={id}"
+        print(query)
+
+        cursor = mysql.connection.cursor()
+        cursor.execute(query)
+        row_headers=[x[0] for x in cursor.description] #this will extract row headers
+        return_value = cursor.fetchone()
+        json_data=[]
+        json_data.append(dict(zip(row_headers,return_value)))
+        print(json_data)
+        if return_json:
+            return json_data
         return jsonify(json_data)
     except Exception as err:
         return jsonify({"status": "failed", "details": str(err)})
@@ -129,6 +154,40 @@ def create_build_failures():
      "md5sum": "uniquemd5sumoffailure"}
     """
     return _insert_item("build_failures")
+
+
+@app.route('/build_failures/<int:id>', methods=['POST'])
+def update_build_failure(id):
+    try:
+        if request.form:
+            data = request.form
+            record = data
+        else:
+            data = request.data.decode('utf8')
+            print(data)
+            record = json.loads(data)
+        _query = "UPDATE build_failures SET "
+        _query += ",".join([f"{key}='{value}'" for key, value in record.items() if key != "id"])
+        _query += f" WHERE id={id}"
+        print(_query)
+
+        cursor = mysql.connection.cursor()
+        cursor.execute(_query)
+        mysql.connection.commit()
+        return jsonify({"status": "success"})
+    except Exception as err:
+        return jsonify({"status": "failed", "details": str(err)})
+
+@app.route('/gui/build_failure/<int:id>', methods=['GET'])
+def gui_build_failure(id):
+    return render_template("failure.html", build_failures=_get_item_by_id("build_failures", id, return_json=True), title=f"Editing failure ID: {id}")
+
+@app.route('/gui/build_failures', methods=['GET'])
+def gui_build_failures():
+    """
+    List all build failures from build_failures table
+    """
+    return render_template("failures.html", build_failures=_list_items("build_failures", return_json=True), title="Failure list")
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=80)
